d5a9e020a66595646413e5f19728c9853aa570e9
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-04 16:11:23 +0200

    Time: 83 ms (93.56%) | Memory: 131.6 MB (7.06%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,62 +1,58 @@
 class Solution {
 private:
-    map<char, vector<int>> make_pref(string &s){
+    vector<vector<int>> make_pref(string &s) {
         int size = int(s.size());
         // freq & pref to know number of freq of any letter in range
-        map<char, vector<int>> pref_on_freq;
-        for (char i = 'a'; i <= 'z'; ++i) {
-            pref_on_freq[ i ].resize(size);
-        }
+        vector<vector<int>> pref_on_freq(26, vector<int>(size, 0));
+
         // freq
         for (int i = 0; i < size; ++i) {
-            pref_on_freq[s[i]][i] = 1;
+            pref_on_freq[s[i] - 'a'][i] = 1;
         }
-        // pref
-        for (char i = 'a'; i <= 'z'; ++i) {
 
+        // pref
+        for (int i = 0; i < 26; ++i) {
             for (int j = 1; j < size; ++j) {
                 pref_on_freq[i][j] += pref_on_freq[i][j - 1];
             }
-
         }
         return pref_on_freq;
     }
 
-    map< char, pair<int, int> > get_first_and_last_appear(string &s){
+    vector<pair<int, int>> get_first_and_last_appear(string &s) {
         int size = int(s.size());
-        map<char, pair<int, int>> first_and_last_appear;
-        for (char i = 'a'; i <= 'z'; ++i) {
-            first_and_last_appear[i] = {-1,-1};
-        }
+        vector<pair<int, int>> first_and_last_appear(26, {-1, -1});
 
         for (int i = 0; i < size; ++i) {
-            auto &[first, last] = first_and_last_appear[s[i]];
-            if( first == -1 )
+            auto &[first, last] = first_and_last_appear[s[i] - 'a'];
+            if (first == -1)
                 first = i, last = i;
             else
                 last = i;
         }
         return first_and_last_appear;
     }
+
 public:
     int countPalindromicSubsequence(string s) {
         int size = int(s.size());
-        map<char, vector<int>> pref_to_each_letter = make_pref(s);
-        map< char, pair<int, int> > first_and_last_appear = get_first_and_last_appear(s);
-        auto freq_in_range = [&](char ch,int l,int r){
-            return pref_to_each_letter[ch][r] - (l? pref_to_each_letter[ch][l - 1] : 0);
-        };
-        int ans = 0;
-        for(auto &[ch, range] : first_and_last_appear){
-            auto &[first, last] = range;
-            if( (first == -1) or (last == first) ) continue;
+        vector<vector<int>> pref_to_each_letter = make_pref(s);
+        vector<pair<int, int>> first_and_last_appear = get_first_and_last_appear(s);
 
-            for (char i = 'a'; i <= 'z'; ++i) {
+        auto freq_in_range = [&](int ch, int l, int r) {
+            return pref_to_each_letter[ch][r] - (l ? pref_to_each_letter[ch][l - 1] : 0);
+        };
+
+        int ans = 0;
+        for (int ch = 0; ch < 26; ++ch) {
+            auto &[first, last] = first_and_last_appear[ch];
+            if ((first == -1) || (last == first)) continue;
+
+            for (int i = 0; i < 26; ++i) {
                 ans += freq_in_range(i, first + 1, last - 1) > 0;
             }
-            
         }
-        
+
         return ans;
     }
 };
