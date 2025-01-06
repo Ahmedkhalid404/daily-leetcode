f5211291edfbac562ac158909a8182122cf31040
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-06 15:57:45 +0200

    Time: 5 ms (54.83%) | Memory: 12.7 MB (8.62%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+		int size = int(boxes.size());
+        vector< int > pref( size + 1 ) , suf( size + 1 );
+
+
+		int ones = (boxes[0] == '1');
+		for(int i = 1; i < size; i ++){
+			pref[i] = pref[i - 1] + ones;
+			ones += (boxes[i] == '1');
+		}
+		ones = (boxes[size - 1] == '1');
+		for(int i = size - 2; ~i ; i--){
+			suf[i] = suf[i + 1] + ones;
+			ones += (boxes[i] == '1');
+		}
+
+		vector< int > ans( size );
+		if( size > 1 ){
+			ans[0] = suf[0];
+			ans[size - 1] = pref[size - 1];
+		}
+
+		for(int i = 1; i < size - 1; i ++){
+			ans[i] = pref[i] + suf[i];
+		}
+
+		return ans;
+
+    }
+};
