f56d04f311fdac6960c31ac755d249cdbf9814bb
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-11 15:17:53 +0200

    Time: 3 ms (72.03%) | Memory: 13.5 MB (91.96%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    bool canConstruct(string &s, int cnt) {
-        if( s.size() < cnt ){
-            return false;
-        }
-
-        array<int, int('z' - 'a' + 1)> freq{};
-
-        for(auto &ch : s){
-            freq[ch - 'a']++;
-        }
-
+    bool canConstruct(string &s, int k) {
+        if(k > s.size()) return false;
+        bitset< 26 > freq;
         int odd = 0;
-        for(auto it : freq){
-            odd += (it & 1);
-            if( odd > cnt ) return false;
+        for(char c : s){
+            freq[ c - 'a' ] = not freq[ c - 'a' ];
+            if( freq[ c - 'a' ] )
+                odd ++;
+            else
+                odd --;
         }
-
-        return true;
+        return odd <= k;
     }
 };
