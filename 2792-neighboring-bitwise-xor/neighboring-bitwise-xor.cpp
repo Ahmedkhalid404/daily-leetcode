a718c42fab6bffa948d6a43ca1c86f73fca8fdbb
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-17 19:45:42 +0200

    Time: 31 ms (8.41%) | Memory: 302.1 MB (5.61%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int n = int(derived.size());
+        if( n == 1 ){
+            return derived[0] == 0;
+        }
+        vector<int> original_0(n), original_1(n);
+        original_0[ 0 ] = 0;
+        original_1[ 0 ] = 1;
+
+
+        auto tryIt = [&](vector<int> original){
+            for (int i = 0; i < n - 1; ++i) {
+                original[i + 1] = derived[i] ^ original[i];
+            }
+
+            return original;
+        };
+
+        auto canBeOriginal = [&](const vector< int > &original){
+            for (int i = 0; i < n; ++i) {
+                int curr = original[i] ^ ( i == n - 1 ? original[0] : original[i + 1] );
+                if( derived[i] != curr )
+                    return false;
+            }
+
+            return true;
+        };
+
+        return (canBeOriginal(tryIt(original_0) ) || canBeOriginal(tryIt(original_1)));
+    }
+};
