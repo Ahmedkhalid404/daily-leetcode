84f50468723a9c1d72ff33e1b56cd692df74a735
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-20 16:36:55 +0200

    Time: 0 ms (100.00%) | Memory: 129.3 MB (100.00%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -5,8 +5,10 @@ public:
         int m = int(mat[0].size());
         int size = n * m;
 
-        unordered_map< int, pair< int, int > > mapping;
-        unordered_map< int , int > row, col;
+        pair< int , int > mapping[ n * m + 1 ];
+        int row[n + 1], col[m + 1];
+        fill(row, row + n + 1, 0);
+        fill(col, col + m + 1, 0);
         
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
@@ -15,15 +17,16 @@ public:
         }
 
         for (int i = 0; i < size; ++i) {
-            
+
             auto [y, x] = mapping[ arr[i] ];
-            row[ y ] --;
-            col[ x ] --;
-            
-            if( (abs( row[y] ) == m) or (abs( col[x] ) == n ) )
+            row[ y ] ++;
+            col[ x ] ++;
+
+
+            if( (row[y] == m) or (col[x] == n) )
                 return i;
         }
-        
+
         assert(false); // we should return before the loop end
         return -1;
     }
