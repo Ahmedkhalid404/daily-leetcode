72a567a30fa2bb075e87604cfab9d919f2550971
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-21 18:24:40 +0200

    Time: 8 ms (24.43%) | Memory: 66.6 MB (76.92%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int size = int(grid[0].size());
-        vector< int64_t > pref_down( size + 1 ), suf_top( size + 1 );
-
-        // pref
-        for (int i = 0; i < size; ++i) {
-            pref_down[i + 1] = pref_down[i] + grid[1][i];
-        }
-
-        // suf
-        for (int i = size - 1; ~i ; --i) {
-            suf_top[i] = suf_top[i + 1] + grid[0][i];
-        }
-
+        int64_t pref_down = 0, suf_top = accumulate(grid[0].begin() + 1, grid[0].end(), 0LL);
         int64_t mx = 1e15;
+        
         for (int i = 0; i < size; ++i) {
-            mx = min({mx, max( pref_down[i], suf_top[i + 1])});
+            mx = min({mx, max( pref_down, suf_top)});
+
+            if( i + 1 < size ) suf_top = suf_top - grid[0][i + 1];
+            pref_down = pref_down + grid[1][i];
         }
 
         return mx;
