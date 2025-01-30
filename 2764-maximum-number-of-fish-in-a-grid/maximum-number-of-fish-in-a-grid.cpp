e836e94b38ca9f1cec91c78b511972b3b63ccf17
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-01-30 16:30:10 +0200

    Time: 7 ms (68.19%) | Memory: 96 MB (70.32%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -2,39 +2,47 @@ class Solution {
 private:
     vector<int> dx = { 1 , -1 , 0 , 0 },
             dy = { 0 , 0 , 1 , -1 };
-    
+
 public:
     int findMaxFish(vector<vector<int>>& grid) {
 
         int n = int(grid.size());
         int m = int(grid.front().size());
 
-        auto valid = [&](int i,int j,int k){
+        auto valid = [&](int i,int j,int k)-> optional<pair<int, int>>{
             int new_i = i + dy[ k ];
             int new_j = j + dx[ k ];
 
-            return (
-                    ( ( new_i < n ) && ( new_i > -1) ) &&
-                    ( ( new_j < m ) && ( new_j > -1 ) ) &&
-                    ( grid[ new_i ][ new_j ] != 0 )
-            );
+            if(
+                ( ( new_i < n ) && ( new_i > -1) ) &&
+                ( ( new_j < m ) && ( new_j > -1 ) ) &&
+                ( grid[ new_i ][ new_j ] != 0 )
+                ){
+                return make_pair(new_i, new_j);
+            }
+
+            return nullopt;
         };
 
         function<int(int,int)> dfs = [&](int i, int j){
+
+            if( (i < 0) or (i >= n) )
+                return 0;
+            if( (j < 0) or (j >= m) )
+                return 0;
+
+
             int val = grid[i][j];
             grid[i][j] = 0; // vis
-            
+
             int sum = val;
 
             for (int k = 0; k < 4; ++k) {
-                if(valid(i, j, k)){
-                    int new_i = i + dy[ k ];
-                    int new_j = j + dx[ k ];
-                    
-                    sum += dfs(new_i, new_j);
+                if(auto pos = valid(i, j, k)){
+                    sum += dfs(pos->first, pos->second);
                 }
             }
-            
+
             return sum;
         };
 
