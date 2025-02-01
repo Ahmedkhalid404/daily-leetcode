f02360ed8fb02d7bbb0dd92e6ed93deb5b9a05b8
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-02-01 05:32:36 +0200

    Time: 0 ms (100.00%) | Memory: 27.8 MB (25.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        bool prev = nums[0] & 1;
-        int sz = int(nums.size());
-        for(int i = 1; i < sz; i ++){
-            if( prev == (nums[i] & 1) )
+        for(int i = 1; i < nums.size(); i ++){
+            if( (nums[i] & 1) == (nums[i - 1] & 1) )
                 return false;
-            prev = !prev; 
         }
         return true;
     }
