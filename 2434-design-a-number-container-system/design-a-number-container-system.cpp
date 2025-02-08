c01db6ff541bad2d293d3e279faacd0dde554e4d
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-02-08 19:09:20 +0200

    Time: 199 ms (5.00%) | Memory: 198.2 MB (6.56%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -0,0 +1,32 @@
+class NumberContainers {
+public:
+    map< int , set< int > > rkm; // each rkm in which index ?
+    map< int , int > idx; // each idx have which number ? 
+    
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        
+        if( idx[ index ] != 0 )
+            rkm[ idx[ index ] ].erase( index );
+            
+        rkm[ number ].insert( index );
+        idx[ index ] = number;
+    }
+    
+    int find(int number) {
+        if( rkm[number].empty() ){
+            return -1;
+        }
+        
+        return *rkm[number].begin();
+    }
+};
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
