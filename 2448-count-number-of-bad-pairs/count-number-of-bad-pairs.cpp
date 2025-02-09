83ae2a0ed892b6fc16e969edffd6f56c835fcc5c
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-02-09 23:42:27 +0200

    Time: 118 ms (13.21%) | Memory: 89.3 MB (41.89%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        map<int, int> freq;
         int64_t ans = 0;
         for(int i = 0; i < nums.size(); i++)
             ans = ans + (i - freq[ nums[i] - i ] ++);
