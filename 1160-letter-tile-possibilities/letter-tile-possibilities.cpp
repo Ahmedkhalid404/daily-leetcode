81a52bbc60ed168f1326e138b12ce20ddfbc880a
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-02-17 19:13:35 +0200

    Time: 2 ms (85.65%) | Memory: 9.1 MB (56.65%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int numTilePossibilities(string tiles) {
-        int freq[26];
-        fill(freq, freq + 26 , 0);
+        unordered_map<char, int> freq;
+        
+        for(auto it : tiles) freq[it] ++;
 
-        for(auto it : tiles) freq[it - 'A'] ++;
         int cnt = 0;
         function<void(int)> rec = [&](int idx){
             if(idx >= tiles.size())
                 return;
             
-            for(int i = 0; i < 26; i ++){
-                if( freq[i] > 0 ){
+            for(auto &it : freq){
+                if( it.second > 0 ){
                     cnt ++;
-                    freq[i] --;
+                    it.second --;
                     rec(idx + 1);
-                    freq[i] ++;
+                    it.second ++;
                 }
             }
         };
