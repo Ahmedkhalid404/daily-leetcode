e73e9f1c5e8260d805976cae421afce45397a548
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-02-19 18:29:23 +0200

    Time: 105 ms (5.05%) | Memory: 118.4 MB (5.05%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int const MAX_N = 10;
+    map< int , vector<string> > happyStrings;
+    char c = [&](){
+
+        function<void(string&&)> generateHappyStrings = [&](string &&curr){
+
+            if(!curr.empty()) happyStrings[curr.size()].push_back(curr);
+
+            if(curr.size() >= MAX_N)
+                return;
+            
+            for(auto &ch : {'a', 'b', 'c'}){
+                if(curr.empty() or curr.back() != ch){
+                    curr.push_back(ch);
+                    generateHappyStrings(move(curr));
+                    curr.pop_back();
+                }
+            }
+        };
+
+        generateHappyStrings(string(""));
+        return char('_');
+    }();
+public:
+    string getHappyString(int n, int k) {
+        if( k > happyStrings[n].size() )
+            return ""s;
+        return happyStrings[n][k - 1];
+    }
+};
