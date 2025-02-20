cf15bf8ce17aede4698417d56f2b8d94af2d6971
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-02-20 22:50:19 +0200

    Time: 0 ms (100.00%) | Memory: 12.6 MB (69.67%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,43 +1,13 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-
-        int n = int(nums.size());
-        unordered_set<string> st;
-        int freq[ n ][2];
-        for(int i = 0; i < n ; i ++){
-            st.insert(nums[i]);
-            for(int j = 0; j < 2; j ++){
-                freq[i][j] = 0;
-            }
+        string ans = "";
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            ans += nums[i][i] == '0' ? '1' : '0';
         }
-
-        for(int i = 0; i < n; i ++){
-            
-            for(int j = 0; j < n; j ++){
-                freq[ j ][ nums[i][j] - '0' ] ++;
-            }
-
-        }
-
-        string res = ""s;
-        for(int i = 0; i < n; i ++){
-            if( freq[i][0] < freq[i][1] )
-                res.push_back('0');
-            else
-                res.push_back('1');
-        }
-
-        for(int i = 0; i < n; i++){
-            if(!st.contains(res)){
-                return res;
-            }
-
-            if(res[i] == '0') res[i] = '1';
-            else res[i] = '0';
-        }
-
-        return ""s;
-    
+        
+        return ans;
     }
 };
