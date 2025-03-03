422f4197d92a4e406fd1fd9b86fb53c1353fc450
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-03-03 09:56:43 +0200

    Time: 32 ms (5.05%) | Memory: 130 MB (70.77%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -3,31 +3,13 @@ public:
     vector<int> pivotArray(vector<int> nums, int pivot) {
         stable_sort(nums.begin(), nums.end(),[&pivot](auto &a, auto &b) {
 
-            if ( a != pivot and b != pivot ) {
-                if ( (a < pivot) and (b < pivot) )
-                    return false;
-                if ( (a > pivot) and (b > pivot) )
-                    return false;
-
-                if ( (a < pivot) and (b > pivot) )
-                    return true;
-
+            if ( a == b )
                 return false;
-            }
-            else {
-                if ( (a == pivot) and (b > pivot) )
-                    return true;
-                if ( (a == pivot) and (b < pivot) )
-                    return false;
 
-                if ( b == pivot ) {
-                    if ( a < pivot )
-                        return true;
-                    if ( a > pivot )
-                        return false;
-                }
-            }
-            return false;
+            if ( (a < pivot and b < pivot) or (a > pivot and b > pivot) )
+                return false;
+
+            return a < b;
         });
         return nums;
     }
