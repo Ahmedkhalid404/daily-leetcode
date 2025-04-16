ccba6c7e1808916ef8e39a869035197129718cea
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-04-16 08:00:00 +0200

    Time: 209 ms (6.16%) | Memory: 83.4 MB (13.98%) - LeetSync

---
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    long long countPairs(int n) { return n * (n - 1LL) / 2; }
+public:
+    long long countGood(vector<int> nums, int k) {
+        int n = nums.size();
+        int l = 0;
+        int r = -1;
+        int cnt = 0;
+        int64_t ans = 0;
+
+        map<int, int> freq;
+
+        while ((r + 1) < n) {
+            r++;
+
+            freq[nums[r]]++;
+            if (freq[nums[r]] >= 2) {
+                cnt = cnt - countPairs(freq[nums[r]] - 1) +
+                      countPairs(freq[nums[r]]);
+            }
+
+            while (cnt >= k) {
+                ans = ans + (n - r);
+
+                freq[nums[l]]--;
+                if (freq[nums[l]]) {
+                    cnt = cnt - countPairs(freq[nums[l]] + 1) +
+                          countPairs(freq[nums[l]]);
+                }
+
+                l++;
+            }
+        }
+
+        return ans;
+    }
+};
