9620e7534374ae8017dfc3b0340983b57c23a767
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-05-03 11:49:12 +0300

    Time: 3 ms (67.46%) | Memory: 115.2 MB (93.75%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -3,36 +3,56 @@ public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         
         int ans = 1e5;
-        for(int i = 1; i <= 6; i++){
-
+       for(int i = 1; i <= 6; i++){
 
             for(int j = 1; j <= 2; j++){
 
                 int cnt = 0;
+                bool flag = true;
                 for(int k = 0; k < tops.size(); k++){
-                    auto &curr = (j == 1 ? tops : bottoms);
-                    auto &other = (j == 1 ? bottoms : tops);
-                    if( curr[k] != i ){
 
-                        if( other[k] != i ){
-                            goto jump;
+                    if( j == 1 ){ // try top
+
+                        if( tops[ k ] != i ){
+                            
+                            if( bottoms[ k ] != i ){
+                                flag = false;
+                                break;
+                            }
+                            //swap
+
+                            cnt ++;
                         }
 
-                        cnt ++;
+                    } else{ // j = 2 -> try bottom
+
+                        if( bottoms[ k ] != i ){
+
+                            if( tops[ k ] != i ){
+                                flag = false;
+                                break;
+                            }
+
+                            cnt ++;
+
+                        }
 
                     }
+
+                }
+                if( flag ){
+                    ans = min(ans, cnt);
                 }
 
-                ans = min(ans, cnt);
 
             }
 
-            jump:;
+       }
 
-
-        }
-
-        return ans == 1e5 ? -1 : ans;
+       if(ans == 1e5)
+            return -1;
+        
+        return ans;
 
     }
 };
