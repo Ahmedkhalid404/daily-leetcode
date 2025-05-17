cb2cb70ccaf5ad13c6096d217e6c1a52444dd440
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-05-17 15:17:26 +0300

    Time: 0 ms (100.00%) | Memory: 11.8 MB (11.80%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int z = 0, o = 0, t = 0;
-        for(int i = 0; i < nums.size() ; i++){
-            if( nums[i] == 0 ) {z++;}
-            if( nums[i] == 1 ) {o++;}
-            if( nums[i] == 2 ) {t++;} 
+        int freq[4] = {};
+        freq[3] = -404;
+        int n = int(nums.size());
+
+        for(int i = 0; i < n; i ++){
+            freq[nums[i]] ++;
         }
 
-        for(int i = 0; i < nums.size(); i++){
+        int val = 0;
+        while( !freq[val] ) val ++;
 
-            if( z > 0 ){
-                nums[i] = 0;
-                z --;
-            } else if ( o > 0 ) {
-                nums[i] = 1;
-                o --;
-            } else if( t > 0 ) {
-                nums[i] = 2;
-                t --;
-            } 
-
+        for(int i = 0; i < n; i ++){
+            nums[i] = val;
+            freq[val] --;
+            while( !freq[val] ) val ++;
         }
 
     }
