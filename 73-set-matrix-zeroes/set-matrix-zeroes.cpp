1f3f6b42438505c5d1d6c4844458a8c8efa01440
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-05-21 11:07:02 +0300

    Time: 1 ms (16.41%) | Memory: 19 MB (6.61%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,55 +4,25 @@ public:
         int n = int(g.size());
         int m = int(g[0].size());
 
-        int a = pow(-2, 31), b = pow(2, 31) - 1;
+        set<int> row, col;
 
-        std::random_device rd;
-        std::mt19937 gen(rd());
-
-        std::uniform_int_distribution<> distrib(a, b);
-
-
-        int notValue = distrib(gen);
-        bool flag = false;
-        while( !flag ){
-            flag = true;
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    if(g[i][j] == notValue){
-                        flag = false;
-                        goto jump;
-                    }
-                }
-            }
-            jump:;
-        }
-
-        cout << notValue << '\n';
         for(int i = 0; i < n; i++){
-
             for(int j = 0; j < m; j++){
-
-                if( !g[i][j] ){
-
-                    for(int k = 0; k < m; k++){ // row
-                        if( g[i][k] ) g[i][k] = notValue;
-                    }
-
-                    for(int k = 0; k < n; k ++){
-                        if(g[k][j]) g[k][j] = notValue;
-                    }
-
+                if(!g[i][j]) {
+                    row.insert(i);
+                    col.insert(j);
                 }
-
             }
-
         }
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(g[i][j] == notValue) g[i][j] = 0;
+                if(row.contains(i) or col.contains(j)) {
+                    g[i][j] = 0;
+                }
             }
         }
+
 
     }
 };
