864357e2b3ae9edce15a31a456d35c21d8eebe16
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2025-10-31 15:50:17 +0200

    Time: 0 ms (100.00%) | Memory: 54.8 MB (17.40%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
@@ -6,7 +6,7 @@ var totalMoney = function(n) {
     let numberOf7s = parseInt(n / 7);
     let remaning = n % 7;
 
-    let sumFrom1ToN = (n) =>  n * (n + 1) / 2;
+    let sumFrom1ToN = n =>  n * (n + 1) / 2;
 
     let sumFrom1To7 = sumFrom1ToN(7);
     let sum = sumFrom1To7 * numberOf7s + (sumFrom1ToN(numberOf7s - 1) * 7);
